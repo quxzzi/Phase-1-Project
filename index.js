e8f2b9eab2b61885cb5d1c05d0b330076918899d
Kevin Dean <kdean1011@gmail.com>
2023-06-13 14:47:21 -0400

    updated api fetch

--- index.js
+++ index.js
@@ -1,17 +1,25 @@
-fetch('https://binaryjazz.us/wp-json/genrenator/v1/genre/', {
-    method: 'POST',
-    headers: {
-        contentType: 'application/json'
-    },
-    body: JSON.stringify({
-        name: genre
+const url = 'https://binaryjazz.us/wp-json/genrenator/v1/genre/';
+// links API 
+function getData(url) {
+  fetch(url)
+    .then(response => response.json())
+    .then(data => {
+        console.log(data);
+//        data(genre => {
+//            const markup = `<li>${genre.____}</li>`;
+//            document.querySelector('resultContainer').insertAdjacentHTML('beforeend', markup);
+//        }) I Don't know how to get the correct data from the API to put on the page. 
     })
-})
- .then(res => {
-    if (res.ok) {
-        console.log('Success')
-    } else {
-        console.log('Error')
-    }
- })
- .then(data => console.log(data))
+};
+// converts HTML to Js objects
+const generateButton = document.getElementById('Generate');
+const resultContainer = document.getElementById('resultContainer');
+
+// event listener for button click
+generateButton.addEventListener('click', async () => {
+  try {
+    const data = await getData(url);
+    resultContainer.textContent = data;
+  } catch (e) {
+}
+});
